807a28eb3614e593197b51fbb3269d2e54bc52c4
İbrahim Aykut BAŞ <ibrahimaykutbas@gmail.com>
2024-09-30 15:46:41 +0300

    feat: Challenge

--- DayForecast/DayForecast/ContentView.swift
+++ DayForecast/DayForecast/ContentView.swift
@@ -1,11 +1,63 @@
 import SwiftUI
 
+struct Day {
+    let day: String
+    let isRaining: Bool
+    let high: Int
+    let low: Int
+}
+
+struct WeatherData {
+    let days: [Day]
+    
+    var averageHighAndLow: (averageHigh: Double, averageLow: Double) {
+        let totalHigh = days.reduce(0) { $0 + $1.high }
+        let totalLow = days.reduce(0) { $0 + $1.low }
+        
+        let averageHigh = Double(totalHigh) / Double(days.count)
+        let averageLow = Double(totalLow) / Double(days.count)
+        
+        return (averageHigh, averageLow)
+    }
+}
+
 struct ContentView: View {
+    let days: [Day] = [
+        Day(day: "Mon", isRaining: true, high: 24, low: 17),
+        Day(day: "Tue", isRaining: true, high: 22, low: 17),
+        Day(day: "Wed", isRaining: true, high: 21, low: 17),
+        Day(day: "Thu", isRaining: false, high: 22, low: 17),
+        Day(day: "Fri", isRaining: false, high: 23, low: 19),
+        Day(day: "Sat", isRaining: false, high: 24, low: 19),
+        Day(day: "Sun", isRaining: false, high: 24, low: 20)
+    ]
+    
+    var weatherData: WeatherData {
+        WeatherData(days: days)
+    }
+    
+    
     var body: some View {
-        HStack {
-            DayForecast(day: "Mon", isRaing: false, high: 70, low: 60)
+        
+        VStack(alignment: .leading) {
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(days, id: \.day) { day in
+                        DayForecast(day: day.day, isRaining: day.isRaining, high: day.high, low: day.low)
+                    }
+                }
+            }
             
-            DayForecast(day: "Tue", isRaing: true, high: 60, low: 50)
+            VStack(alignment: .leading) {
+                Text("Average High: \(weatherData.averageHighAndLow.averageHigh, specifier: "%.2f")")
+                    .font(.system(size: 24))
+                    .fontWeight(.semibold)
+                
+                Text("Average Low: \(weatherData.averageHighAndLow.averageLow, specifier: "%.2f")")
+                    .font(.system(size: 24))
+                    .fontWeight(.semibold)
+            }
+            .padding(.horizontal)
         }
     }
 }
@@ -16,12 +68,12 @@ struct ContentView: View {
 
 struct DayForecast: View {
     let day: String
-    let isRaing: Bool
+    let isRaining: Bool
     let high: Int
     let low: Int
     
     var iconName: String {
-        if isRaing {
+        if isRaining {
             return "cloud.rain.fill"
         } else {
             return "sun.max.fill"
@@ -29,10 +81,10 @@ struct DayForecast: View {
     }
     
     var iconColor: Color {
-        if isRaing {
-            return .blue
+        if isRaining {
+            return high < 22 ? .gray : .cyan
         } else {
-            return .yellow
+            return high > 23 ? .orange : .yellow
         }
     }
     
